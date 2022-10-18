e0b2a0feb31007318a9f53b88d521fda7639059d
Robert <4060573+rarDevelopment@users.noreply.github.com>
2022-10-18 01:20:18 -0400

    get full path for food.json

--- ChefBot/ChefBot/FoodRepository.cs
+++ ChefBot/ChefBot/FoodRepository.cs
@@ -12,7 +12,8 @@ namespace ChefBot
 
         public FoodItem[]? GetAllFood()
         {
-            using StreamReader r = new StreamReader("./food.json");
+            string filePath = Path.GetFullPath("food.json");
+            using StreamReader r = new StreamReader(filePath);
             string json = r.ReadToEnd();
             var food = JsonSerializer.Deserialize<FoodData>(json, new JsonSerializerOptions
             {
